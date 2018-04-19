df4d088a3650a45cbfc5655c12528c050121a1d3
EtsuN <32398296+EtsuN@users.noreply.github.com>
2018-04-19 13:51:03 -0600

    Add files via upload

---
+++ Multi-Col/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/CTestTestfile.cmake
@@ -0,0 +1,25 @@
+# CMake generated Testfile for 
+# Source directory: /home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV
+# Build directory: /home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_absolute_pose "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_absolute_pose")
+set_tests_properties(test_absolute_pose PROPERTIES  WORKING_DIRECTORY "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin")
+add_test(test_absolute_pose_sac "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_absolute_pose_sac")
+add_test(test_noncentral_absolute_pose "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_noncentral_absolute_pose")
+add_test(test_noncentral_absolute_pose_sac "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_noncentral_absolute_pose_sac")
+add_test(test_multi_noncentral_absolute_pose_sac "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_multi_noncentral_absolute_pose_sac")
+add_test(test_relative_pose "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_relative_pose")
+add_test(test_relative_pose_rotationOnly "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_relative_pose_rotationOnly")
+add_test(test_relative_pose_rotationOnly_sac "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_relative_pose_rotationOnly_sac")
+add_test(test_relative_pose_sac "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_relative_pose_sac")
+add_test(test_noncentral_relative_pose "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_noncentral_relative_pose")
+add_test(test_noncentral_relative_pose_sac "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_noncentral_relative_pose_sac")
+add_test(test_multi_noncentral_relative_pose_sac "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_multi_noncentral_relative_pose_sac")
+add_test(test_eigensolver_sac "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_eigensolver_sac")
+add_test(test_triangulation "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_triangulation")
+add_test(test_eigensolver "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_eigensolver")
+add_test(test_point_cloud "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_point_cloud")
+add_test(test_point_cloud_sac "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_point_cloud_sac")
+add_test(test_Sturm "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/ThirdParty/OpenGV/build/bin/test_Sturm")
