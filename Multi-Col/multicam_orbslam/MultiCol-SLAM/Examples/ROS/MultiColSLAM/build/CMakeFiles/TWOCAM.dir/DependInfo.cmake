d64bf3adb37e1de1a345034a4a4869b1b3d4de42
EtsuN <32398296+EtsuN@users.noreply.github.com>
2018-04-19 12:51:36 -0600

    Add files via upload

---
+++ Multi-Col/multicam_orbslam/MultiCol-SLAM/Examples/ROS/MultiColSLAM/build/CMakeFiles/TWOCAM.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/Examples/ROS/MultiColSLAM/src/ros_sync_twocam.cc" "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/Examples/ROS/MultiColSLAM/build/CMakeFiles/TWOCAM.dir/src/ros_sync_twocam.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_PACKAGE_NAME=\"MultiColSLAM\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "devel/include"
+  "../include"
+  "/opt/ros/kinetic/include"
+  "/opt/ros/kinetic/include/opencv-3.2.0-dev"
+  "/opt/ros/kinetic/include/opencv-3.2.0-dev/opencv"
+  "../"
+  "../../../.."
+  "../../../../include"
+  "/home/e4e/workspace/multicam_orbslam/Pangolin/include"
+  "/home/e4e/workspace/multicam_orbslam/Pangolin/build/src/include"
+  "/usr/include/eigen3"
+  ".././../../../ThirdParty/g2o"
+  ".././../../../ThirdParty/DBoW2"
+  ".././../../../ThirdParty/OpenGV"
+  ".././../../../ThirdParty/Eigen"
+  ".././../../../ThirdParty"
+  ".././../../../ThirdParty/OpenGV/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Multi-Col/multicam_orbslam/MultiCol-SLAM/Examples/ROS/MultiColSLAM/build/CMakeFiles/TWOCAM.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../msg_gen"
+  "../msg_gen"
+  "../src/MultiColSLAM/msg"
+  "CMakeFiles/TWOCAM.dir/src/ros_sync_twocam.cc.o"
+  "devel/lib/MultiColSLAM/TWOCAM.pdb"
+  "devel/lib/MultiColSLAM/TWOCAM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TWOCAM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
