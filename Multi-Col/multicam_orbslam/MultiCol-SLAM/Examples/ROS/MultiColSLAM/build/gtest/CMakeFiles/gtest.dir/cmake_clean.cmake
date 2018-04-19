d21900931d77a4c009ce570c514caa172b6abf26
EtsuN <32398296+EtsuN@users.noreply.github.com>
2018-04-19 12:36:05 -0600

    Add files via upload

---
+++ Multi-Col/multicam_orbslam/MultiCol-SLAM/Examples/ROS/MultiColSLAM/build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/Examples/ROS/MultiColSLAM/lib/libgtest.pdb"
+  "/home/e4e/workspace/multicam_orbslam/multicam_orbslam/MultiCol-SLAM/Examples/ROS/MultiColSLAM/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
